bc6b1159ac825d2cf5de9bcb0a6cdc3e9b4252cd
Kafouille@gmail.com <Kafouille@gmail.com@d1139d92-4d4e-2ec3-fc3e-6e4027237e5b>
2011-02-11 23:46:03 +0000

    Removing files not meant to be in Trunk
    git-svn-id: https://acf.googlecode.com/svn/trunk@6 d1139d92-4d4e-2ec3-fc3e-6e4027237e5b

--- lua/entities/ACF_Battlepod_Avatar/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Base 					= "base_ai" 
-ENT.Type 					= "ai"
-
-ENT.Spawnable				= false
-ENT.AdminSpawnable			= true
-ENT.AutomaticFrameAdvance 	= true
-ENT.Model					= "models/Humans/Group03/male_02.mdl"
-
-util.PrecacheModel( ENT.Model )
