ef911387cdfc076ede6df46319558d9d8ae82bea
Kafouille@gmail.com <Kafouille@gmail.com@d1139d92-4d4e-2ec3-fc3e-6e4027237e5b>
2011-02-17 12:18:14 +0000

    Fixed ACF vehicles not spawning
    git-svn-id: http://acf.googlecode.com/svn/trunk@34 d1139d92-4d4e-2ec3-fc3e-6e4027237e5b

--- lua/autorun/battlepod.lua
+++ lua/autorun/battlepod.lua
@@ -8,58 +8,40 @@ end
 
 local Category = "Armoured Combat Framework"
 
--- local V =  {
-				-- Name = "Standard Driver Pod", 
-				-- Class = "prop_vehicle_prisoner_pod",
-				-- Category = Category,
+local V =  {
+	Name = "Standard Driver Pod",
+	Class = "prop_vehicle_prisoner_pod",
+	Category = Category,
 
-				-- Author = "Lazermaniac",
-				-- Information = "Modified prisonpod for more realistic player damage",
-				-- Model = "models/vehicles/driver_pod.mdl",
-				-- KeyValues = {
-								-- vehiclescript	=	"scripts/vehicles/prisoner_pod.txt",
-								-- limitview		=	"0"
-							-- }
-				-- Members = {
-								-- HandleAnimation = HandleACFPodAnimation,
-				-- }
--- }
--- list.Set( "Vehicles", "acf_pod", V )
-
--- local V = { 	
-				-- // Required information
-				-- Name = "Standard Pilot Seat", 
-				-- Class = "prop_vehicle_prisoner_pod",
-				-- Category = Category,
-
-				-- // Optional information
-				-- Author = "Lazermaniac",
-				-- Information = "A generic seat for accurate damage modelling.",
-				-- Model = "models/vehicles/pilot_seat.mdl",
-				-- KeyValues = {
-								-- vehiclescript	=	"scripts/vehicles/prisoner_pod.txt",
-								-- limitview		=	"0"
-							-- }
-				-- Members = {
-								-- HandleAnimation = HandleACFSeatAnimation,
-							-- }
--- }
--- list.Set( "Vehicles", "acf_pilotseat", V )
+	Author = "Lazermaniac",
+	Information = "Modified prisonpod for more realistic player damage",
+	Model = "models/vehicles/driver_pod.mdl",
+	KeyValues = {
+					vehiclescript	=	"scripts/vehicles/prisoner_pod.txt",
+					limitview		=	"0"
+				},
+	Members = {
+					HandleAnimation = HandleACFPodAnimation
+	}
+}
+list.Set( "Vehicles", "acf_pod", V )
 
 local V = { 	
-				// Required information
-				Name = "Airboat Seat", 
-				Class = "acf_battlepod",
-				Category = Category,
-				
-				// Optional information
-				Author = "Kafouille",
-				Information = "A generic seat for accurate damage modelling.",
-				Model = "models/vehicles/pilot_seat.mdl",
+	// Required information
+	Name = "Standard Pilot Seat",
+	Class = "prop_vehicle_prisoner_pod",
+	Category = Category,
 
-				KeyValues = {
-					model = "models/vehicles/pilot_seat.mdl",
+	// Optional information
+	Author = "Lazermaniac",
+	Information = "A generic seat for accurate damage modelling.",
+	Model = "models/vehicles/pilot_seat.mdl",
+	KeyValues = {
+					vehiclescript	=	"scripts/vehicles/prisoner_pod.txt",
+					limitview		=	"0"
+				},
+	Members = {
+					HandleAnimation = HandleACFSeatAnimation
 				}
 }
-
-list.Set( "Vehicles", "acf_battleseat", V )
+list.Set( "Vehicles", "acf_pilotseat", V )
