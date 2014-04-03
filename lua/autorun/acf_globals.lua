050117b557166e397241d765fc864b644695ba64
Fervidusletum <fervidusletum@gmail.com>
2014-04-02 23:37:34 -0700

    Short-Barrel Cannon Adjustments
    Gun weight decreased a bit to account for heavier shells. RoF changed
    slightly.

--- lua/autorun/acf_globals.lua
+++ lua/autorun/acf_globals.lua
@@ -2,7 +2,7 @@ ACF = {}
 ACF.AmmoTypes = {}
 ACF.MenuFunc = {}
 ACF.AmmoBlacklist = {}
-ACF.Version = 488 -- REMEMBER TO CHANGE THIS FOR GODS SAKE, OMFG!!!!!!! -wrex
+ACF.Version = 489 -- REMEMBER TO CHANGE THIS FOR GODS SAKE, OMFG!!!!!!! -wrex
 ACF.CurrentVersion = 0 -- just defining a variable, do not change
 
 ACF.Threshold = 225	--Health Divisor
